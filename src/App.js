917ce3c364f3801bae7ac52c5074657e6c1ca535
GulhayoSayfullayeva <gulhayo.sayfullayeva@gmail.com>
2024-01-15 22:57:09 +0100

    errors are fixed

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
 import CitySearch from './components/CitySearch';
+import { Alert } from './components/Alert';
 import EventList from './components/EventList';
 import NumberOfEvents from './components/NumberOfEvents';
 import { extractLocations, getEvents } from './api';
@@ -11,6 +12,7 @@ function App() {
   const [noe, setNoe] = useState(32);
   const [allLocations, setAllLocations] = useState([]);
   const [currentCity, setCurrentCity] = useState('See all cities');
+  const [infoAlert, setInfoAlert] = useState('');
 
   
 
@@ -27,6 +29,9 @@ function App() {
   }
   return (
     <div className="App">
+      <div className="alerts-container">
+        {infoAlert.length ? <Alert text={infoAlert}/> : null}
+      </div>
       <CitySearch allLocations={allLocations} setCurrentCity={setCurrentCity}/>
       <NumberOfEvents setNumberOfEvents={setNoe} />
       <EventList events={events}/>
